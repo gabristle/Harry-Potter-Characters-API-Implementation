e18ea2b10b665580ca46cd4c09370e512f53fdee
Gabriella Costa <gabx2205@gmail.com>
2024-03-23 15:46:05 -0300

    reset display

--- script.js
+++ script.js
@@ -20,7 +20,8 @@ function searchCharacter(){
     xhr.send();
 }
 
-function displayCharacter(character){ 
+function displayCharacter(character){
+    displayReset();
     var descName = document.getElementById('desc--name');
     var descSpecies = document.getElementById('desc--species');
     var descGender = document.getElementById('desc--gender');
@@ -45,6 +46,29 @@ function displayCharacter(character){
 }
 
 function displayError(character){
+    displayReset();
     var errorMessage = document.getElementById('search--error');
     errorMessage.textContent = `The character you searched don't exist`;
+}
+
+function displayReset(){
+    var descName = document.getElementById('desc--name');
+    var descSpecies = document.getElementById('desc--species');
+    var descGender = document.getElementById('desc--gender');
+    var descHouse = document.getElementById('desc--house');
+    var descDateOfBirth = document.getElementById('desc--date-of-birth');
+    var descAncestry = document.getElementById('desc--ancestry');
+    var descEye = document.getElementById('desc--eye-colour');
+    var descHair = document.getElementById('desc--hair-colour');
+    var img = document.getElementById('desc--img');
+    descName.textContent = '';
+    descSpecies.textContent = '';
+    descGender.textContent = '';
+    descHouse.textContent = '';
+    descDateOfBirth.textContent = '';
+    descAncestry.textContent = '';
+    descEye.textContent = '';
+    descHair.textContent = '';
+    img.src = '';
+    img.alt = '';
 }
